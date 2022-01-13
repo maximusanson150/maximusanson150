158c7dac8d60f8078d20d1ba164e186a7a55fe2f
maximusanson150 <89879708+maximusanson150@users.noreply.github.com>
2022-01-13 10:37:13 +0100

    Update loop.ps1

--- .github/workflows/loop.ps1
+++ .github/workflows/loop.ps1
@@ -1,6 +1,6 @@
 $i = 360
 do {
-    Write-Host $i
+    tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
     Sleep 60
     $i--
 } while ($i -gt 0)
